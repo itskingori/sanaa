62c5e62691a9b1aeb78baca9ede4774dee79e7e0
King'ori Maina <j@kingori.co>
2017-10-17 20:46:27 +0200

    Initialise CLI aspect of the tool
    Set up the entry point without any sub-commands. It actually does't do
    anything at this point so it's really just a skeleton. There's a basic
    attempt at setting up a configuration file but that's all
    auto-generated from cobra's generator.

---
+++ cmd/root.go
@@ -0,0 +1,67 @@
+// Copyright © 2017 Job King'ori Maina <j@kingori.co>
+//
+// This file is part of sanaa.
+//
+// sanaa is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// sanaa is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with sanaa. If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "sanaa",
+	Short: "A HTML to PDF/Image conversion microservice",
+	Long: `A HTML to PDF/Image conversion microservice powered by wkhtmltopdf,
+wkhtmltoimage and xvfb.`,
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "location of configuration file")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".config") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+	viper.AutomaticEnv()           // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
