c410c1edfb39fcf8ce54811f1c41c6e7dbd3e670
King'ori Maina <j@kingori.co>
2018-02-24 04:28:15 +0200

    Add S3 bucket region configurability via flags

--- cmd/worker.go
+++ cmd/worker.go
@@ -58,11 +58,10 @@ func init() {
 	// Add flags to workerCmd
 	workerCmd.PersistentFlags().Int("concurrency", 2, "number of conversion jobs that can be processed at a time, maximum is 10")
 	workerCmd.PersistentFlags().String("s3-bucket", "", "the name of the S3 bucket to use when storing rendered files ")
+	workerCmd.PersistentFlags().String("s3-region", "us-east-1", "the region of the S3 bucket used to store rendered files")
 
 	// Bind workerCmd flags with viper configuration
 	viper.BindPFlag("worker.concurrency", workerCmd.PersistentFlags().Lookup("concurrency"))
 	viper.BindPFlag("worker.s3_bucket", workerCmd.PersistentFlags().Lookup("s3-bucket"))
-
-	// Flag defaults
-	viper.SetDefault("worker.s3_region", "us-east-1")
+	viper.BindPFlag("worker.s3_region", workerCmd.PersistentFlags().Lookup("s3-region"))
 }
